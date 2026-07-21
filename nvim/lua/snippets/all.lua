ca40d43649c7cdc32dddc15d4cb900331d924581
Qczer <119138392+Qczer@users.noreply.github.com>
2026-07-22 00:34:37 +0200

    feat: Update nvim snippets

---
+++ nvim/lua/snippets/all.lua
@@ -0,0 +1,10 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local t = ls.text_node
+
+return {
+  s('mail', t('mkuczera@proton.me')),
+  s('email', t('mkuczera@proton.me')),
+  s('gmail', t('mateuszkuczera3@gmail.com')),
+  s('gh', t('github.com/Qczer'))
+}
