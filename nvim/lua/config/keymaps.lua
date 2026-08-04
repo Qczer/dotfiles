70dbc082f1c27aea31df2cca06705804e6dcda08
Qczer <119138392+Qczer@users.noreply.github.com>
2026-08-04 12:45:38 +0200

    feat: Add debugging to nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
+local dap = require('dap')
 local map = vim.keymap.set
 
 map('n', '<C-d>', '<C-d>zz')
@@ -13,7 +13,6 @@ map({ 'n', 'v', 'o' }, "'", '`', { noremap = true })
 map({ 'n', 'v', 'o' }, '`', "'", { noremap = true })
 
 map('n', '<leader>fs', vim.cmd.ClangdSwitchSourceHeader, { desc = 'Switch Source/Header' })
-map('n', '<leader>go', '<cmd>!gh repo view --web<CR>', { desc = 'Open Repo' })
 map('n', '<leader>r', vim.lsp.buf.rename, { desc = 'Rename symbol' })
 map('n', '<leader>f/', Snacks.picker.grep, { desc = 'Grep (include all)' })
 map('n', '<leader>/', function()
@@ -28,6 +27,18 @@ map('n', '<leader>/', function()
     },
   }
 end, { desc = 'Grep (ignore)' })
+
+-- Debugging
+vim.keymap.set('n', '<F5>', dap.continue)
+vim.keymap.set('n', '<F10>', dap.step_over)
+vim.keymap.set('n', '<F11>', dap.step_into)
+vim.keymap.set('n', '<F12>', dap.step_out)
+
+vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, { desc = 'Toggle breakpoint' })
+vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Open Debug REPL' })
+vim.keymap.set('n', '<leader>du', function()
+  require('dapui').toggle()
+end, { desc = 'Toggle Debug UI' })
 
 map('i', 'jj', '<Esc>')
 map('i', 'jk', '<Esc>')
---
+++ nvim/lua/plugins/tools.lua
@@ -0,0 +1,70 @@
+return {
+  {
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    opts = {
+      ensure_installed = {
+        'clang-format',
+        'codelldb',
+      },
+    },
+  },
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'nvim-neotest/nvim-nio',
+      'theHamsta/nvim-dap-virtual-text',
+      'jay-babu/mason-nvim-dap.nvim',
+    },
+    config = function()
+      local dap = require('dap')
+
+      require('mason-nvim-dap').setup({
+        ensure_installed = {
+          'codelldb',
+        },
+        automatic_installation = true,
+      })
+
+      require('dapui').setup()
+      require('nvim-dap-virtual-text').setup()
+
+      dap.adapters.codelldb = {
+        type = 'server',
+        port = '${port}',
+        executable = {
+          command = vim.fn.stdpath('data') .. '/mason/bin/codelldb',
+          args = { '--port', '${port}' },
+        },
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = 'Launch C++',
+          type = 'codelldb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = false,
+        },
+      }
+
+      dap.configurations.c = dap.configurations.cpp
+      dap.configurations.rust = dap.configurations.cpp
+
+      dap.listeners.after.event_initialized['dapui'] = function()
+        require('dapui').open()
+      end
+
+      dap.listeners.before.event_terminated['dapui'] = function()
+        require('dapui').close()
+      end
+
+      dap.listeners.before.event_exited['dapui'] = function()
+        require('dapui').close()
+      end
+    end,
+  },
+}
